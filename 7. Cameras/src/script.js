3b04978199f1aa8a2486e40bc18ce1b4cc149c40
Veeno <sebastien.cloiseau@gmail.com>
2023-10-21 01:43:32 +0200

    training

--- 7. Cameras/src/script.js
+++ 7. Cameras/src/script.js
@@ -2,38 +2,41 @@ import * as THREE from "three";
 
 const canvas = document.querySelector("canvas.webgl");
 
+const scene = new THREE.Scene();
+
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
 
-const scene = new THREE.Scene();
-
+// Objects
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: "rebeccapurple" });
+const material = new THREE.MeshBasicMaterial({ color: "orange" });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 5;
+camera.position.y = 3;
 camera.position.x = 2;
-camera.position.y = 2;
 camera.lookAt(mesh.position);
 scene.add(camera);
 
-const render = new THREE.WebGLRenderer({
+const rendered = new THREE.WebGLRenderer({
   canvas,
 });
-render.setSize(sizes.width, sizes.height);
+
+rendered.setSize(sizes.width, sizes.height);
 
 const clock = new THREE.Clock();
 
-const frame = () => {
+const tick = () => {
   const elapsedTime = clock.getElapsedTime();
-  mesh.rotation.x = elapsedTime;
   mesh.rotation.y = elapsedTime;
-  render.render(scene, camera);
-  window.requestAnimationFrame(frame);
+  mesh.rotation.x = elapsedTime;
+  rendered.render(scene, camera);
+
+  window.requestAnimationFrame(tick);
 };
 
-frame();
+tick();
