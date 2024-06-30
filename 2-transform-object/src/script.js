303cd8de3509bf0916f020c238e692724ee316df
Veeno <sebastien.cloiseau@gmail.com>
2024-06-30 07:22:54 +0200

    feature: buffer attribute

--- 2-transform-object/src/script.js
+++ 2-transform-object/src/script.js
@@ -1,40 +1,43 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-// Canvas
 const canvas = document.querySelector("canvas.webgl");
 
 // Scene
 const scene = new THREE.Scene();
-const group = new THREE.Group();
 
-const cube1 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: "rgb(234,143,25)" })
-);
+// OBJECT
+// const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
 
-const cube2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: "rgb(100,143,250)" })
-);
+const geometry = new THREE.BufferGeometry();
+const count = 50;
 
-cube1.rotation.reorder("YXZ");
-cube1.rotation.x = Math.PI * 0.25;
+const positionArr = new Float32Array(count * 3 * 3);
+for (let i = 0; i < positionArr.length; i++) {
+  positionArr[i] = Math.random() - 0.5;
+}
 
-group.add(cube1);
-scene.add(group);
+const positionAtributes = new THREE.BufferAttribute(positionArr, 3);
+geometry.setAttribute("position", positionAtributes);
 
-// mesh.position.x = 0.7;
-// mesh.position.y = -0.6;
-// mesh.position.z = 0.2;
-
-// Axis helper
-const axesHelper = new THREE.AxesHelper();
-scene.add(axesHelper);
+const material = new THREE.MeshBasicMaterial({
+  color: "grey",
+  wireframe: true,
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 const sizes = {
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 const cursor = {
   x: 0,
@@ -46,19 +49,23 @@ window.addEventListener("mousemove", (e) => {
   cursor.y = -(e.clientY / sizes.height - 0.5);
 });
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
-// camera.position.set(2, 1, 4);
-scene.add(camera);
+window.addEventListener("resize", (e) => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-// console.log(mesh.position.distanceTo(camera.position));
-
-// console.log(camera.position);
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 const renderer = new THREE.WebGLRenderer({
   canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 let time = Date.now();
 
@@ -66,16 +73,10 @@ const tick = () => {
   const currentTime = Date.now();
   const deltaTime = currentTime - time;
   time = currentTime;
+  mesh.rotation.y += deltaTime * 0.001;
 
-  // Update camera
-  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-  camera.position.y = cursor.y * 5;
-  camera.lookAt(new THREE.Vector3());
-
+  controls.update();
   renderer.render(scene, camera);
-
   window.requestAnimationFrame(tick);
 };
-
 tick();
